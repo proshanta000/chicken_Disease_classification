2c1f813cd9eb0586d027442ffa40ee4d880d6cef
Prashanta Kumar Mithu <proshanta.mithu5@gmail.com>
2025-09-24 02:03:02 +0600

    aaded requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ ="0.0.0"
+
+REPO_NAME = 'chicken_Disease_classification'
+AUTHOR_USER_NAME = "proshanta000"
+SRC_REPO = "CNNCDC"
+AUTHOR_EMAIL = "proshanta.mithu5@gmail.com,"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content_type= "text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
